dcde5547a50ca3afc8be3bef29c727b98731ff9a
Azrael Park <azrael.park@nexr.com>
2013-11-20 10:34:21 +0900

    NOOZIE-19 Compatable with NHIVE-39

--- core/src/main/java/org/apache/oozie/action/hive/HiveTClientV2.java
+++ core/src/main/java/org/apache/oozie/action/hive/HiveTClientV2.java
@@ -25,7 +25,7 @@ public class HiveTClientV2 implements HiveTClient {
     private transient OperationHandle operation;
 
     private transient TableSchema schema;
-    private transient Iterator<Row> iterator;
+    private transient Iterator<Object[]> iterator;
 
     public HiveTClientV2(HiveConnection connection, Utils.JdbcConnectionParams params) throws Exception {
         this.params = params;
@@ -122,7 +122,7 @@ public class HiveTClientV2 implements HiveTClient {
         while (result.size() < numRows) {
             if (iterator == null || !iterator.hasNext()) {
                 RowSet rows = client.fetchResults(operation, FetchOrientation.FETCH_NEXT, numRows - result.size());
-                iterator = rows.getRows().iterator();
+                iterator = rows.iterator();
             }
             if (!iterator.hasNext()) {
                 break;
@@ -182,12 +182,11 @@ public class HiveTClientV2 implements HiveTClient {
         }
     }
 
-    private String toString(Row row) throws Exception {
+    private String toString(Object[] row) throws Exception {
         List<ColumnDescriptor> columns = schema.getColumnDescriptors();
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < columns.size(); i++) {
-            ColumnValue value = row.getValues().get(i);
-            Object eval = value.getColumnValue(columns.get(i).getType());
+            Object eval = RowSet.evaluate(columns.get(i), row[i]);
             if (builder.length() > 0) {
                 builder.append(" ");
             }
