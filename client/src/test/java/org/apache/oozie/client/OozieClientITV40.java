45db9c8fdbbc9d71e1ad9336a4af2d38c138e6e5
Azrael Park <azrael.park@nexr.com>
2014-04-16 00:11:05 +0900

    NOOZIE-91 Add test cases for v4.0

---
+++ client/src/test/java/org/apache/oozie/client/OozieClientITV40.java
@@ -0,0 +1,70 @@
+package org.apache.oozie.client;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Properties;
+
+public class OozieClientITV40 extends OozieClientIT{
+    
+    /**
+     * Test capture std-out, std-err from shell action .
+     *
+     */
+    @Test
+    public void testShellOutStreamV40() {
+        try {
+            Properties configs = getDefaultProperties();
+
+            // ehco
+            String appName = "shell-outstream";
+            String version = "v40";
+            String appPath = baseAppPath + "/" + version + "/" + appName;
+            configs.put(OozieClient.APP_PATH, appPath);
+            configs.put("appName", appName);
+            configs.put("version", version);
+
+            uploadApps(appPath, appName, version, "script-outstream.sh");
+
+            String jobID = run(configs);
+            String status = "";
+            String capture = "";
+            try {
+                for (int i = 0; i < 50; i++) {
+                    WorkflowJob wfJob = getClient().getJobInfo(jobID);
+                    LOG.debug(wfJob.getId() + " [" + wfJob.getStatus().toString() + "]");
+                    List<WorkflowAction> actionList = wfJob.getActions();
+                    for (WorkflowAction action : actionList) {
+                        if(action.getName().equals("shell-v40")){
+                            LOG.debug("    " + action.getName() + " [" + action.getStatus().toString() + "]");
+                            LOG.debug("    " + "capture >> \n " + action.getData() + "\n");
+                            capture = action.getData();
+                        }
+                    }
+                    status = wfJob.getStatus().toString();
+                    if (wfJob.getStatus().equals(WorkflowJob.Status.SUCCEEDED)
+                            || wfJob.getStatus().equals(WorkflowJob.Status.KILLED)
+                            || wfJob.getStatus().equals(WorkflowJob.Status.FAILED)) {
+                        break;
+                    }
+                    Thread.sleep(POLLING);
+                }
+            } catch (Exception e) {
+                LOG.debug("Fail to monitor : " + jobID, e);
+            }
+
+            LOG.info("DONE JOB >> " + jobID + " [" + status + "]");
+
+            Assert.assertEquals(WorkflowJob.Status.SUCCEEDED.toString(), status);
+
+            //output file is changed in JavaActionExecutor
+            //Assert.assertTrue(capture.contains("hello-standard-output"));
+        } catch (Exception e) {
+            LOG.info("Fail to testShellOutStreamV40", e);
+            Assert.fail();
+        }
+        LOG.info("    >>>> Pass testShellOutStreamV40 \n");
+    }
+
+}
