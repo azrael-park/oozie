6b66432b00a818d37e0ccf40f1235d7fd0fa80a9
Azrael Park <azrael.park@nexr.com>
2014-07-15 14:37:18 +0900

    NOOZIE-99 EL paring error should be non-transient error.

--- client/src/test/java/org/apache/oozie/client/OozieClientITV40.java
+++ client/src/test/java/org/apache/oozie/client/OozieClientITV40.java
@@ -67,4 +67,64 @@ public class OozieClientITV40 extends OozieClientIT{
         LOG.info("    >>>> Pass testShellOutStreamV40 \n");
     }
 
+    /**
+     * Test capture std-out, std-err from shell action .
+     *
+     */
+    @Test
+    public void testShellELParsingErrorHandleV40() {
+        try {
+            Properties configs = getDefaultProperties();
+
+            // ehco
+            String appName = "shell-el-error";
+            String version = "v40";
+            String appPath = baseAppPath + "/" + version + "/" + appName;
+            configs.put(OozieClient.APP_PATH, appPath);
+            configs.put("appName", appName);
+            configs.put("version", version);
+
+            uploadApps(appPath, appName, version, "script-outstream.sh");
+
+            String jobID = run(configs);
+            String status = "";
+            String actionStatus = "";
+            try {
+                for (int i = 0; i < 50; i++) {
+                    WorkflowJob wfJob = getClient().getJobInfo(jobID);
+                    LOG.debug(wfJob.getId() + " [" + wfJob.getStatus().toString() + "]");
+                    List<WorkflowAction> actionList = wfJob.getActions();
+                    for (WorkflowAction action : actionList) {
+                        if(action.getName().equals("shell-v40")){
+                            LOG.debug("    " + action.getName() + " [" + action.getStatus().toString() + "]");
+                            actionStatus = action.getStatus().toString();
+                        }
+                    }
+                    status = wfJob.getStatus().toString();
+                    if (wfJob.getStatus().equals(WorkflowJob.Status.SUCCEEDED)
+                            || wfJob.getStatus().equals(WorkflowJob.Status.KILLED)
+                            || wfJob.getStatus().equals(WorkflowJob.Status.FAILED)) {
+                        break;
+                    }
+                    if (actionStatus.equals(WorkflowAction.Status.START_MANUAL.toString())) {
+                        break;
+                    }
+                    Thread.sleep(POLLING);
+                }
+            } catch (Exception e) {
+                LOG.debug("Fail to monitor : " + jobID, e);
+            }
+
+            LOG.info("DONE JOB >> " + jobID + " [" + status + "]");
+
+            Assert.assertEquals(WorkflowJob.Status.RUNNING.toString(), status);
+            Assert.assertEquals(WorkflowAction.Status.START_MANUAL.toString(), actionStatus);
+
+        } catch (Exception e) {
+            LOG.info("Fail to testShellELErrorHandleV40", e);
+            Assert.fail();
+        }
+        LOG.info("    >>>> Pass testShellELErrorHandleV40 \n");
+    }
+
 }
