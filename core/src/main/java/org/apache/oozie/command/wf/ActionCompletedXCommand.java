2f5c7ca4b10925e4ee5a7d6d7225fbff1d21873d
Azrael Park <azrael.park@nexr.com>
2015-02-22 17:23:17 +0900

    OOZIE-1918 ActionXCommand refactoring for code reuse

--- core/src/main/java/org/apache/oozie/command/wf/ActionCompletedXCommand.java
+++ core/src/main/java/org/apache/oozie/command/wf/ActionCompletedXCommand.java
@@ -27,6 +27,7 @@ import org.apache.oozie.command.CommandException;
 import org.apache.oozie.command.PreconditionException;
 import org.apache.oozie.executor.jpa.WorkflowActionQueryExecutor;
 import org.apache.oozie.executor.jpa.WorkflowActionQueryExecutor.WorkflowActionQuery;
+import org.apache.oozie.executor.jpa.WorkflowJobQueryExecutor;
 import org.apache.oozie.service.ActionService;
 import org.apache.oozie.service.Services;
 import org.apache.oozie.util.LogUtils;
@@ -35,18 +36,15 @@ import org.apache.oozie.util.ParamChecker;
 /**
  * This command is executed once the Workflow command is finished.
  */
-public class CompletedActionXCommand extends WorkflowXCommand<Void> {
-    private final String actionId;
+public class ActionCompletedXCommand extends ActionXCommand<Void> {
     private final String externalStatus;
-    private WorkflowActionBean wfactionBean;
 
-    public CompletedActionXCommand(String actionId, String externalStatus, Properties actionData, int priority) {
-        super("callback", "callback", priority);
-        this.actionId = ParamChecker.notEmpty(actionId, "actionId");
+    public ActionCompletedXCommand(String actionId, String externalStatus, Properties actionData, int priority) {
+        super(actionId, "action.callback", "action.callback", priority);
         this.externalStatus = ParamChecker.notEmpty(externalStatus, "externalStatus");
     }
 
-    public CompletedActionXCommand(String actionId, String externalStatus, Properties actionData) {
+    public ActionCompletedXCommand(String actionId, String externalStatus, Properties actionData) {
         this(actionId, externalStatus, actionData, 1);
     }
 
@@ -62,14 +60,7 @@ public class CompletedActionXCommand extends WorkflowXCommand<Void> {
      */
     @Override
     protected void eagerLoadState() throws CommandException {
-        try {
-            this.wfactionBean = WorkflowActionQueryExecutor.getInstance().get(WorkflowActionQuery.GET_ACTION_COMPLETED,
-                    this.actionId);
-        }
-        catch (Exception ex) {
-            throw new CommandException(ErrorCode.E0603, ex.getMessage(), ex);
-        }
-        LogUtils.setLogInfo(this.wfactionBean);
+        loadActionBean(WorkflowJobQueryExecutor.WorkflowJobQuery.GET_WORKFLOW, WorkflowActionQuery.GET_ACTION_COMPLETED);
     }
 
     /*
@@ -79,8 +70,8 @@ public class CompletedActionXCommand extends WorkflowXCommand<Void> {
      */
     @Override
     protected void eagerVerifyPrecondition() throws CommandException, PreconditionException {
-        if (this.wfactionBean.getStatus() != WorkflowActionBean.Status.RUNNING) {
-            throw new CommandException(ErrorCode.E0800, actionId, this.wfactionBean.getStatus());
+        if (this.wfAction.getStatus() != WorkflowActionBean.Status.RUNNING) {
+            throw new CommandException(ErrorCode.E0800, actionId, this.wfAction.getStatus());
         }
     }
 
@@ -91,11 +82,11 @@ public class CompletedActionXCommand extends WorkflowXCommand<Void> {
      */
     @Override
     protected Void execute() throws CommandException {
-        ActionExecutor executor = Services.get().get(ActionService.class).getExecutor(this.wfactionBean.getType());
+        ActionExecutor executor = Services.get().get(ActionService.class).getExecutor(this.wfAction.getType());
         // this is done because oozie notifications (of sub-wfs) is send
         // every status change, not only on completion.
         if (executor.isCompleted(externalStatus)) {
-            queue(new ActionCheckXCommand(this.wfactionBean.getId(), getPriority(), -1));
+            queue(new ActionCheckXCommand(this.wfAction.getId(), getPriority(), -1));
         }
         return null;
     }
